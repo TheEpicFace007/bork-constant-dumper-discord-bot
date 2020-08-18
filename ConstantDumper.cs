37221fd189aa3446b9cfbcd95c7f636b39457866
Vini Dalvino <vinidalvino@github.com>
2020-08-18 14:57:47 -0400

    Finished making the ConstantDumper.cs

--- ConstantDumper.cs
+++ ConstantDumper.cs
@@ -1,18 +1,29 @@
-﻿namespace auto_server_side_fuck
+﻿using RestSharp;
+
+namespace auto_server_side_fuck
 {
     public class ConstantDumper
     {
         public string ScriptToConstantDump { get; set; }
-        public string Constants { get; }
+        public string Constants { get; set; }
 
         public ConstantDumper(string scriptToConstantDump)
         {
             ScriptToConstantDump = scriptToConstantDump;
         }
 
+        public ConstantDumper()
+        {
+        }
+
         public string DumpConstants()
         {
-            
+            var client = new RestClient("http://borks.club:2095/dumper");
+            var request = new RestRequest(RestSharp.Method.POST);
+            request.AddParameter("code",ScriptToConstantDump);
+            var response = client.Execute(request);
+            Constants = response.Content;
+            return response.Content;
         }
     }
 }
