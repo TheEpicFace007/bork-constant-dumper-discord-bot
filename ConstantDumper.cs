f59b914e9555a7ab366f46f8789ccbc5cb4fcd4b
Vini Dalvino <vinidalvino@github.com>
2020-08-18 14:27:55 -0400

    init commit

---
+++ ConstantDumper.cs
@@ -0,0 +1,18 @@
+﻿namespace auto_server_side_fuck
+{
+    public class ConstantDumper
+    {
+        public string ScriptToConstantDump { get; set; }
+        public string Constants { get; }
+
+        public ConstantDumper(string scriptToConstantDump)
+        {
+            ScriptToConstantDump = scriptToConstantDump;
+        }
+
+        public string DumpConstants()
+        {
+            
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using MoonSharp.Interpreter;
+
+namespace auto_server_side_fuck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
